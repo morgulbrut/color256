f40587fe1864547781f8671724d9d8eddb3111a0
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-10-31 13:22:55 +0100

    Moved formats to formats.go

--- Formats.go
+++ Formats.go
@@ -5,49 +5,49 @@ package color256
 
 import "fmt"
 
-//---------------------------------------------------------------------
-// Bold
-//---------------------------------------------------------------------
+// Bold returns a bold string.
+func Bold(format string, a ...interface{}) string {
+	return fmt.Sprintf("[%dm%s[22m", FmtBold, fmt.Sprintf(format, a...))
+}
 
+// Faint returns a faint string.
+func Faint(format string, a ...interface{}) string {
+	return fmt.Sprintf("[%dm%s[22m", FmtFaint, fmt.Sprintf(format, a...))
+}
+
+// Italic returns an italic string.
+func Italic(format string, a ...interface{}) string {
+	return fmt.Sprintf("[%dm%s[23m", FmtItalic, fmt.Sprintf(format, a...))
+}
+
+// Underlined returns an underlined string.
+func Underlined(format string, a ...interface{}) string {
+	return fmt.Sprintf("[%dm%s[24m", FmtUnderlined, fmt.Sprintf(format, a...))
+}
+
+// Reversed returns an inversed string.
+func Reversed(format string, a ...interface{}) string {
+	return fmt.Sprintf("[%dm%s[27m", FmtReversed, fmt.Sprintf(format, a...))
+}
 // PrintBold is a convenient helper function to print a bold text.
 // A newline is appended to format by default.
 func PrintBold(format string, a ...interface{}) { fmt.Println(Bold(format,a...)) }
-
-//---------------------------------------------------------------------
-// Bright
-//---------------------------------------------------------------------
 
 // PrintBright is a convenient helper function to print a bright text.
 // A newline is appended to format by default.
 func PrintBright(format string, a ...interface{}) { fmt.Println(Bright(format,a...)) }
 
-//---------------------------------------------------------------------
-// Faint
-//---------------------------------------------------------------------
-
 // PrintFaint is a convenient helper function to print a faint text.
 // A newline is appended to format by default.
 func PrintFaint(format string, a ...interface{}) { fmt.Println(Faint(format,a...)) }
-
-//---------------------------------------------------------------------
-// Italic
-//---------------------------------------------------------------------
 
 // PrintItalic is a convenient helper function to print a italic text.
 // A newline is appended to format by default.
 func PrintItalic(format string, a ...interface{}) { fmt.Println(Italic(format,a...)) }
 
-//---------------------------------------------------------------------
-// Underlined
-//---------------------------------------------------------------------
-
 // PrintUnderlined is a convenient helper function to print a underlined text.
 // A newline is appended to format by default.
 func PrintUnderlined(format string, a ...interface{}) { fmt.Println(Underlined(format,a...)) }
-
-//---------------------------------------------------------------------
-// Reversed
-//---------------------------------------------------------------------
 
 // PrintReversed is a convenient helper function to print a reversed text.
 // A newline is appended to format by default.
