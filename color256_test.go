a14a4d31a8caccfc37069a07d9194a71ce14e2aa
Tillo Bosshart <tillo.bosshart@kubeg.ch>
2019-10-31 13:22:11 +0100

    Tests restructurerd

--- color256_test.go
+++ color256_test.go
@@ -1,60 +1,169 @@
+
 package color256
 
 import (
 	"fmt"
 	"testing"
 )
-
-func TestManual(t *testing.T) {
-	Init()
-	fmt.Println(Red("Red") + " and " + Green("Green"))
-	fmt.Println(Red(Italic("Red Italic")))
-	fmt.Println(Red(Bold("Red Bold")))
-	fmt.Println(Red(Bold(Italic("Red Bold Italic"))))
-	fmt.Println(Red(Reversed("Red Bold Reversed")))
-
-	fmt.Println(Orange("Orange") + " and " + Green("Green"))
-	fmt.Println(Orange(Italic("Orange Italic")))
-	fmt.Println(Orange(Bold("Orange Bold")))
-	fmt.Println(Orange(Bold(Italic("Orange Bold Italic"))))
-	fmt.Println(Orange(Reversed("Orange Bold Reversed")))
-
-	fmt.Println(HiOrange("HiOrange") + " and " + Green("Green"))
-	fmt.Println(HiOrange(Italic("HiOrange Italic")))
-	fmt.Println(HiOrange(Bold("HiOrange Bold")))
-	fmt.Println(HiOrange(Bold(Italic("HiOrange Bold Italic"))))
-	fmt.Println(HiOrange(Reversed("HiOrange Bold Reversed")))
-
-	fmt.Println(Yellow("Yellow") + " and " + Green("Green"))
-	fmt.Println(Yellow(Italic("Yellow Italic")))
-	fmt.Println(Yellow(Bold("Yellow Bold")))
-	fmt.Println(Yellow(Bold(Italic("Yellow Bold Italic"))))
-	fmt.Println(Yellow(Reversed("Yellow Bold Reversed")))
-
-	fmt.Println(HiYellow("HiYellow") + " and " + Green("Green"))
-	fmt.Println(HiYellow(Italic("HiYellow Italic")))
-	fmt.Println(HiYellow(Bold("HiYellow Bold")))
-	fmt.Println(HiYellow(Bold(Italic("HiYellow Bold Italic"))))
-	fmt.Println(HiYellow(Reversed("HiYellow Bold Reversed")))
-
-	fmt.Println(Color(Xd70087, "DJENNEO"))
-	fmt.Println()
-
-	fmt.Println(Bold("Bold"))
-	fmt.Println(Faint("Faint"))
-	fmt.Println(Italic("Italic"))
-	fmt.Println(Underlined("Underlined"))
-	fmt.Println(Reversed("Reversed"))
-
-	fmt.Println(Bold(Green("Green") + " and Bold"))
-	fmt.Println(Green("Green and " + Bold("Bold") + " and Green"))
-
-	fmt.Println(Bright(Green("Green") + " and Bright"))
-	fmt.Println(Green("Green and " + Bright("Bright") + " and Green"))
-
-	fmt.Println(Faint(Green("Green") + " and Faint"))
-	fmt.Println(Green("Green and " + Faint("Faint") + " and Green"))
-
-	fmt.Println(White(Bold(BgGreen("Green") + " and Bold")))
-	fmt.Println(Black(BgGreen("Green and " + Bold("Bold") + " and Green")))
+func TestBlack(t *testing.T) {
+fmt.Println(Black("Black and "+Bold("Bold")  + " and Black"))
+fmt.Println(Black("Black and "+Bright("Bright")  + " and Black"))
+fmt.Println(Black("Black and "+Faint("Faint")  + " and Black"))
+fmt.Println(Black("Black and "+Italic("Italic")  + " and Black"))
+fmt.Println(Black("Black and "+Underlined("Underlined")  + " and Black"))
+fmt.Println(Black("Black and "+Reversed("Reversed")  + " and Black"))
 }
+
+func TestRed(t *testing.T) {
+fmt.Println(Red("Red and "+Bold("Bold")  + " and Red"))
+fmt.Println(Red("Red and "+Bright("Bright")  + " and Red"))
+fmt.Println(Red("Red and "+Faint("Faint")  + " and Red"))
+fmt.Println(Red("Red and "+Italic("Italic")  + " and Red"))
+fmt.Println(Red("Red and "+Underlined("Underlined")  + " and Red"))
+fmt.Println(Red("Red and "+Reversed("Reversed")  + " and Red"))
+}
+
+func TestGreen(t *testing.T) {
+fmt.Println(Green("Green and "+Bold("Bold")  + " and Green"))
+fmt.Println(Green("Green and "+Bright("Bright")  + " and Green"))
+fmt.Println(Green("Green and "+Faint("Faint")  + " and Green"))
+fmt.Println(Green("Green and "+Italic("Italic")  + " and Green"))
+fmt.Println(Green("Green and "+Underlined("Underlined")  + " and Green"))
+fmt.Println(Green("Green and "+Reversed("Reversed")  + " and Green"))
+}
+
+func TestYellow(t *testing.T) {
+fmt.Println(Yellow("Yellow and "+Bold("Bold")  + " and Yellow"))
+fmt.Println(Yellow("Yellow and "+Bright("Bright")  + " and Yellow"))
+fmt.Println(Yellow("Yellow and "+Faint("Faint")  + " and Yellow"))
+fmt.Println(Yellow("Yellow and "+Italic("Italic")  + " and Yellow"))
+fmt.Println(Yellow("Yellow and "+Underlined("Underlined")  + " and Yellow"))
+fmt.Println(Yellow("Yellow and "+Reversed("Reversed")  + " and Yellow"))
+}
+
+func TestBlue(t *testing.T) {
+fmt.Println(Blue("Blue and "+Bold("Bold")  + " and Blue"))
+fmt.Println(Blue("Blue and "+Bright("Bright")  + " and Blue"))
+fmt.Println(Blue("Blue and "+Faint("Faint")  + " and Blue"))
+fmt.Println(Blue("Blue and "+Italic("Italic")  + " and Blue"))
+fmt.Println(Blue("Blue and "+Underlined("Underlined")  + " and Blue"))
+fmt.Println(Blue("Blue and "+Reversed("Reversed")  + " and Blue"))
+}
+
+func TestMagenta(t *testing.T) {
+fmt.Println(Magenta("Magenta and "+Bold("Bold")  + " and Magenta"))
+fmt.Println(Magenta("Magenta and "+Bright("Bright")  + " and Magenta"))
+fmt.Println(Magenta("Magenta and "+Faint("Faint")  + " and Magenta"))
+fmt.Println(Magenta("Magenta and "+Italic("Italic")  + " and Magenta"))
+fmt.Println(Magenta("Magenta and "+Underlined("Underlined")  + " and Magenta"))
+fmt.Println(Magenta("Magenta and "+Reversed("Reversed")  + " and Magenta"))
+}
+
+func TestCyan(t *testing.T) {
+fmt.Println(Cyan("Cyan and "+Bold("Bold")  + " and Cyan"))
+fmt.Println(Cyan("Cyan and "+Bright("Bright")  + " and Cyan"))
+fmt.Println(Cyan("Cyan and "+Faint("Faint")  + " and Cyan"))
+fmt.Println(Cyan("Cyan and "+Italic("Italic")  + " and Cyan"))
+fmt.Println(Cyan("Cyan and "+Underlined("Underlined")  + " and Cyan"))
+fmt.Println(Cyan("Cyan and "+Reversed("Reversed")  + " and Cyan"))
+}
+
+func TestWhite(t *testing.T) {
+fmt.Println(White("White and "+Bold("Bold")  + " and White"))
+fmt.Println(White("White and "+Bright("Bright")  + " and White"))
+fmt.Println(White("White and "+Faint("Faint")  + " and White"))
+fmt.Println(White("White and "+Italic("Italic")  + " and White"))
+fmt.Println(White("White and "+Underlined("Underlined")  + " and White"))
+fmt.Println(White("White and "+Reversed("Reversed")  + " and White"))
+}
+
+func TestHiBlack(t *testing.T) {
+fmt.Println(HiBlack("HiBlack and "+Bold("Bold")  + " and HiBlack"))
+fmt.Println(HiBlack("HiBlack and "+Bright("Bright")  + " and HiBlack"))
+fmt.Println(HiBlack("HiBlack and "+Faint("Faint")  + " and HiBlack"))
+fmt.Println(HiBlack("HiBlack and "+Italic("Italic")  + " and HiBlack"))
+fmt.Println(HiBlack("HiBlack and "+Underlined("Underlined")  + " and HiBlack"))
+fmt.Println(HiBlack("HiBlack and "+Reversed("Reversed")  + " and HiBlack"))
+}
+
+func TestHiRed(t *testing.T) {
+fmt.Println(HiRed("HiRed and "+Bold("Bold")  + " and HiRed"))
+fmt.Println(HiRed("HiRed and "+Bright("Bright")  + " and HiRed"))
+fmt.Println(HiRed("HiRed and "+Faint("Faint")  + " and HiRed"))
+fmt.Println(HiRed("HiRed and "+Italic("Italic")  + " and HiRed"))
+fmt.Println(HiRed("HiRed and "+Underlined("Underlined")  + " and HiRed"))
+fmt.Println(HiRed("HiRed and "+Reversed("Reversed")  + " and HiRed"))
+}
+
+func TestHiGreen(t *testing.T) {
+fmt.Println(HiGreen("HiGreen and "+Bold("Bold")  + " and HiGreen"))
+fmt.Println(HiGreen("HiGreen and "+Bright("Bright")  + " and HiGreen"))
+fmt.Println(HiGreen("HiGreen and "+Faint("Faint")  + " and HiGreen"))
+fmt.Println(HiGreen("HiGreen and "+Italic("Italic")  + " and HiGreen"))
+fmt.Println(HiGreen("HiGreen and "+Underlined("Underlined")  + " and HiGreen"))
+fmt.Println(HiGreen("HiGreen and "+Reversed("Reversed")  + " and HiGreen"))
+}
+
+func TestHiYellow(t *testing.T) {
+fmt.Println(HiYellow("HiYellow and "+Bold("Bold")  + " and HiYellow"))
+fmt.Println(HiYellow("HiYellow and "+Bright("Bright")  + " and HiYellow"))
+fmt.Println(HiYellow("HiYellow and "+Faint("Faint")  + " and HiYellow"))
+fmt.Println(HiYellow("HiYellow and "+Italic("Italic")  + " and HiYellow"))
+fmt.Println(HiYellow("HiYellow and "+Underlined("Underlined")  + " and HiYellow"))
+fmt.Println(HiYellow("HiYellow and "+Reversed("Reversed")  + " and HiYellow"))
+}
+
+func TestHiBlue(t *testing.T) {
+fmt.Println(HiBlue("HiBlue and "+Bold("Bold")  + " and HiBlue"))
+fmt.Println(HiBlue("HiBlue and "+Bright("Bright")  + " and HiBlue"))
+fmt.Println(HiBlue("HiBlue and "+Faint("Faint")  + " and HiBlue"))
+fmt.Println(HiBlue("HiBlue and "+Italic("Italic")  + " and HiBlue"))
+fmt.Println(HiBlue("HiBlue and "+Underlined("Underlined")  + " and HiBlue"))
+fmt.Println(HiBlue("HiBlue and "+Reversed("Reversed")  + " and HiBlue"))
+}
+
+func TestHiMagenta(t *testing.T) {
+fmt.Println(HiMagenta("HiMagenta and "+Bold("Bold")  + " and HiMagenta"))
+fmt.Println(HiMagenta("HiMagenta and "+Bright("Bright")  + " and HiMagenta"))
+fmt.Println(HiMagenta("HiMagenta and "+Faint("Faint")  + " and HiMagenta"))
+fmt.Println(HiMagenta("HiMagenta and "+Italic("Italic")  + " and HiMagenta"))
+fmt.Println(HiMagenta("HiMagenta and "+Underlined("Underlined")  + " and HiMagenta"))
+fmt.Println(HiMagenta("HiMagenta and "+Reversed("Reversed")  + " and HiMagenta"))
+}
+
+func TestHiCyan(t *testing.T) {
+fmt.Println(HiCyan("HiCyan and "+Bold("Bold")  + " and HiCyan"))
+fmt.Println(HiCyan("HiCyan and "+Bright("Bright")  + " and HiCyan"))
+fmt.Println(HiCyan("HiCyan and "+Faint("Faint")  + " and HiCyan"))
+fmt.Println(HiCyan("HiCyan and "+Italic("Italic")  + " and HiCyan"))
+fmt.Println(HiCyan("HiCyan and "+Underlined("Underlined")  + " and HiCyan"))
+fmt.Println(HiCyan("HiCyan and "+Reversed("Reversed")  + " and HiCyan"))
+}
+
+func TestHiWhite(t *testing.T) {
+fmt.Println(HiWhite("HiWhite and "+Bold("Bold")  + " and HiWhite"))
+fmt.Println(HiWhite("HiWhite and "+Bright("Bright")  + " and HiWhite"))
+fmt.Println(HiWhite("HiWhite and "+Faint("Faint")  + " and HiWhite"))
+fmt.Println(HiWhite("HiWhite and "+Italic("Italic")  + " and HiWhite"))
+fmt.Println(HiWhite("HiWhite and "+Underlined("Underlined")  + " and HiWhite"))
+fmt.Println(HiWhite("HiWhite and "+Reversed("Reversed")  + " and HiWhite"))
+}
+
+func TestOrange(t *testing.T) {
+fmt.Println(Orange("Orange and "+Bold("Bold")  + " and Orange"))
+fmt.Println(Orange("Orange and "+Bright("Bright")  + " and Orange"))
+fmt.Println(Orange("Orange and "+Faint("Faint")  + " and Orange"))
+fmt.Println(Orange("Orange and "+Italic("Italic")  + " and Orange"))
+fmt.Println(Orange("Orange and "+Underlined("Underlined")  + " and Orange"))
+fmt.Println(Orange("Orange and "+Reversed("Reversed")  + " and Orange"))
+}
+
+func TestHiOrange(t *testing.T) {
+fmt.Println(HiOrange("HiOrange and "+Bold("Bold")  + " and HiOrange"))
+fmt.Println(HiOrange("HiOrange and "+Bright("Bright")  + " and HiOrange"))
+fmt.Println(HiOrange("HiOrange and "+Faint("Faint")  + " and HiOrange"))
+fmt.Println(HiOrange("HiOrange and "+Italic("Italic")  + " and HiOrange"))
+fmt.Println(HiOrange("HiOrange and "+Underlined("Underlined")  + " and HiOrange"))
+fmt.Println(HiOrange("HiOrange and "+Reversed("Reversed")  + " and HiOrange"))
+}
+
--- genHelperGo.py
+++ genHelperGo.py
@@ -82,16 +82,53 @@ with open("Formats.go", "w") as file:
     file.write("package color256\n\n")
     file.write('import "fmt"\n\n')
 
-    for f in formats:   
-        file.write("//---------------------------------------------------------------------\n")
-        file.write("// " + f + "\n")
-        file.write("//---------------------------------------------------------------------\n\n")
+    funcs = '''// Bold returns a bold string.
+func Bold(format string, a ...interface{}) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[22m", FmtBold, fmt.Sprintf(format, a...))
+}
 
+// Faint returns a faint string.
+func Faint(format string, a ...interface{}) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[22m", FmtFaint, fmt.Sprintf(format, a...))
+}
+
+// Italic returns an italic string.
+func Italic(format string, a ...interface{}) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[23m", FmtItalic, fmt.Sprintf(format, a...))
+}
+
+// Underlined returns an underlined string.
+func Underlined(format string, a ...interface{}) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[24m", FmtUnderlined, fmt.Sprintf(format, a...))
+}
+
+// Reversed returns an inversed string.
+func Reversed(format string, a ...interface{}) string {
+	return fmt.Sprintf("\x1b[%dm%s\x1b[27m", FmtReversed, fmt.Sprintf(format, a...))
+}
+'''
+    file.write(funcs)
+
+    for f in formats:   
         file.write("// Print" + f +" is a convenient helper function to print a " + f.lower()+" text.\n")
         file.write("// A newline is appended to format by default.\n")
         file.write("func Print" + f +"(format string, a ...interface{}) { fmt.Println("+f+"(format,a...)) }\n\n")
 
-        '''
-        file.write("//" +f+ " is a convenient to get a " + f.lower()+" string.\n")
-        file.write("func " +f+ "(format string, a ...interface{}) string { return Format(Fmt" +f+", fmt.Sprintf(format,a...))}\n\n")
-        '''
+
+with open("color256_test.go", "w") as file:
+    print("color256_test.go")
+    test = '''
+package color256
+
+import (
+	"fmt"
+	"testing"
+)
+'''   
+    file.write(test)
+    
+    for col in cols:
+        file.write("func Test"+col+"(t *testing.T) {\n")
+        for f in formats:
+            file.write("fmt.Println("+col+"(\""+col+" and \"+"+f +"(\""+f+"\")  + \" and "+col+"\"))\n")
+        file.write("}\n\n")
